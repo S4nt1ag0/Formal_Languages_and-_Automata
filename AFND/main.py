2661bd7262cee7e6d389f42b9812e33529d04e64
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-05-17 02:17:54 -0300

    primeiro commit do trabalho 2

---
+++ AFND/main.py
@@ -0,0 +1,49 @@
+from random import randint
+from typing import List
+
+states = input().split(" ");  # pegando os estados
+alfabeth = input().split(" ")  # pegando o alfabeto
+
+stateOfError = randint(0, len(states) * 100)
+
+dicionarioDeEstadosFuturos = dict();
+
+trasaction = int(input())  # pegando o numero de transações
+
+
+for i in range(trasaction):
+    tripla = input().split(" ");  # pegando a tripla
+    stateFrom = tripla[0];
+    letra = tripla[1];
+    stateTo = tripla[2];
+    dicionarioDeEstadosFuturos[stateFrom, letra] = dicionarioDeEstadosFuturos.get((stateFrom, letra)) or []
+    dicionarioDeEstadosFuturos[stateFrom, letra].append(stateTo);  # populando o dicionario com os estados futuros corretos
+
+stack_of_atualStates: List[str] = []
+
+initialStates = input().split(" ");
+finalStates = input().split(" ");
+
+words = input().split(" ")
+
+stack_of_futureStates: List[str] = []
+for word in words:
+    for initialState in initialStates:
+        stack_of_atualStates.append(initialState)
+    for y in word:
+        while stack_of_atualStates:
+            atualState = stack_of_atualStates.pop()
+            futureStates = dicionarioDeEstadosFuturos.get((atualState,y))
+            if(futureStates):
+                for futureState in futureStates:
+                    stack_of_futureStates.append(futureState)
+        while stack_of_futureStates:
+            stack_of_atualStates.append(stack_of_futureStates.pop())
+        flag = 0
+        for finalState in finalStates:
+            if(finalState in stack_of_atualStates):
+                flag = 1
+    if (flag):
+        print('S')
+    else:
+        print('N')
