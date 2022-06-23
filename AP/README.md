6ebbfc62d1f23dca4d1c3b59f8c13ec44bb74b14
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-06-22 22:25:40 -0300

    first commit AP

---
+++ AP/README.md
@@ -0,0 +1,28 @@
+# Formal Languages and Automata
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)
+
+***
+> Gustavo Santiago Sousa <br>
+> Bachelor of Exact and Technological Sciences - UFRB <br>
+> Graduating in Computer Engineering - UFRB <br>
+
+- [Getting Started](#Getting_Started)
+  - [requisites](#requisites)
+  - [Running the application](#Running_the_application) 
+- [About The Project](#aboutProject)
+  - [States and Alfabeth](#States_and_Alfabeth)
+  - [Dictionary](#dictionary)
+  - [Feeding dictionary](#feeding_dictionary)
+  - [Final definitions](#final_definitions)
+  - [Word analysis](#word_analysis)
+***
+## <a id="Getting_Started" />Getting Started
+### <a id="requisites" />Requisites
+- [Python 3.6](https://www.python.org/downloads/release/python-360/)
+### <a id="Running_the_application" />Running the application
+
+```sh
+python main.py
+```
+<a id="aboutProject" />About The Project
+- 
---
+++ AP/main.py
@@ -0,0 +1,48 @@
+states = input().split(" ");  # pegando os estados
+alfabeth = input().split(" ")  # pegando o alfabeto
+
+trasaction = int(input())  # pegando o numero de transações
+
+dicionarioDeEstadosFuturos = {};
+
+for state in states:
+    dicionarioDeEstadosFuturos[state] = {} #criando um dicionario para cada estado
+
+for i in range(0, trasaction):
+    tripla = input().split(" ");  # pegando a tripla
+    stateFrom = tripla[0];
+    letra = tripla[1];
+    stateTo = tripla[2];
+
+    if(letra in dicionarioDeEstadosFuturos[stateFrom]): #adiciona um estado a lista de estados futuros
+        dicionarioDeEstadosFuturos[stateFrom][letra].append(stateTo)
+    else: #ou cria a lista caso essa ainda não exista
+        dicionarioDeEstadosFuturos[stateFrom][letra] = [stateTo]
+
+stack_of_atualStates = []
+
+initialState = input()
+finalStates = input().split(" ");
+
+words = input().split(" ")
+
+for word in words:
+    stack_of_atualStates = [initialState]
+    for letter in word:
+        stack_of_futureStates = []
+        for state in stack_of_atualStates:
+            if(dicionarioDeEstadosFuturos[state].get(letter)):
+                futureStates = dicionarioDeEstadosFuturos[state].get(letter)
+                for futureState in futureStates:
+                    if(futureState not in stack_of_futureStates):
+                        stack_of_futureStates.append(futureState)
+        stack_of_atualStates = stack_of_futureStates
+
+    flag = 0
+    for state in stack_of_atualStates:
+        if(state in finalStates):
+            flag = 1
+    if(flag):
+        print('S')
+    else:
+        print('N')
