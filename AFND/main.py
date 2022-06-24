8e3b1f8bf8e2d9ed145152d820aaefe558857d7f
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-06-23 22:51:49 -0300

    v1

--- AFND/main.py
+++ AFND/main.py
@@ -1,84 +1,48 @@
-estados = input().split(" ") # pegando os estados
-alfabeto = input().split(" ")  # pegando o alfabeto
-pilhaAlfabeto = input().split(" ")  # pegando o alfabeto da pilha
-transacoes = int(input())  # pegando o numero de transações
+states = input().split(" ");  # pegando os estados
+alfabeth = input().split(" ")  # pegando o alfabeto
 
-dicionarioDeEstadosFuturos = {}
+trasaction = int(input())  # pegando o numero de transações
 
-for estado in estados:
-    dicionarioDeEstadosFuturos[estado] = {}  # criando um dicionario para cada estado
+dicionarioDeEstadosFuturos = {};
 
-for i in range(0, transacoes):
-    quintupla = input().split(" ") # pegando a tripla
-    estadoFrom = quintupla[0]
-    letra = quintupla[1]
-    desempilha = quintupla[2]
-    estadoTo = quintupla[3]
-    empilha = quintupla[4]
+for state in states:
+    dicionarioDeEstadosFuturos[state] = {}  # criando um dicionario para cada estado
 
-    if letra not in dicionarioDeEstadosFuturos[estadoFrom]:  # adiciona um estado a lista de estados futuros
-        dicionarioDeEstadosFuturos[estadoFrom][letra] = []
+for i in range(0, trasaction):
+    tripla = input().split(" ");  # pegando a tripla
+    stateFrom = tripla[0];
+    letra = tripla[1];
+    stateTo = tripla[2];
 
-    dicionarioDeEstadosFuturos[estadoFrom][letra].append([desempilha, estadoTo, empilha])
+    if (letra in dicionarioDeEstadosFuturos[stateFrom]):  # adiciona um estado a lista de estados futuros
+        dicionarioDeEstadosFuturos[stateFrom][letra].append(stateTo)
+    else:  # ou cria a lista caso essa ainda não exista
+        dicionarioDeEstadosFuturos[stateFrom][letra] = [stateTo]
 
-estadoInicial = input()
-estadosFinais = input().split(" ")
+stack_of_atualStates = []
 
-palavras = input().split(" ")
+initialState = input()
+finalStates = input().split(" ");
 
-for palavra in palavras:
-    pilhaEstadosAtuais = [(estadoInicial, [], palavra)]
+words = input().split(" ")
+
+for word in words:
+    stack_of_atualStates = [initialState]
+    for letter in word:
+        stack_of_futureStates = []
+        for state in stack_of_atualStates:
+            if (dicionarioDeEstadosFuturos[state].get(letter)):
+                futureStates = dicionarioDeEstadosFuturos[state].get(letter)
+                for futureState in futureStates:
+                    if (futureState not in stack_of_futureStates):
+                        stack_of_futureStates.append(futureState)
+        stack_of_atualStates = stack_of_futureStates
+
     flag = 0
-
-    while flag == 0 and len(palavra) > 0 and len(pilhaEstadosAtuais) > 0:
-        estado, pilha, palavraTemp = pilhaEstadosAtuais.pop()
-
-        if dicionarioDeEstadosFuturos[estado].get('*') and palavraTemp != '*':
-            triplas = dicionarioDeEstadosFuturos[estado].get('*')
-            for desempilha, estadoTo, empilha in triplas:
-                pilhaTemporaria = pilha.copy()
-
-                if desempilha != '*':
-                    if len(pilhaTemporaria) == 0:
-                        break
-                    else:
-                        topo = pilhaTemporaria.pop()
-                        if desempilha != topo:
-                            continue
-
-                if empilha != '*':
-                    pilhaTemporaria.append(empilha)
-
-                pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria,palavraTemp])
-
-        if len(palavraTemp) == 0:
-            if estado in estadosFinais and len(pilha) == 0:
-                flag = 1
-                break
-            else:
-                continue
-
-        palavraTemp2 = palavraTemp[1:]
-
-        if dicionarioDeEstadosFuturos[estado].get(palavraTemp[0]):
-            triplas = dicionarioDeEstadosFuturos[estado].get(palavraTemp[0])
-            for desempilha, estadoTo, empilha in triplas:
-                pilhaTemporaria = pilha.copy()
-
-                if desempilha != '*':
-                    if len(pilhaTemporaria) == 0:
-                        break
-                    else:
-                        topo = pilhaTemporaria.pop()
-                        if desempilha != topo:
-                            continue
-
-                if empilha != '*':
-                    pilhaTemporaria.append(empilha)
-
-                pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria, palavraTemp2])
-
-    if flag:
+    for state in stack_of_atualStates:
+        if (state in finalStates):
+            flag = 1
+    if (flag):
         print('S')
     else:
         print('N')
--- AP/main.py
+++ AP/main.py
@@ -1,48 +1,84 @@
-states = input().split(" ");  # pegando os estados
-alfabeth = input().split(" ")  # pegando o alfabeto
+estados = input().split(" ") # pegando os estados
+alfabeto = input().split(" ")  # pegando o alfabeto
+pilhaAlfabeto = input().split(" ")  # pegando o alfabeto da pilha
+transacoes = int(input())  # pegando o numero de transações
 
-trasaction = int(input())  # pegando o numero de transações
+dicionarioDeEstadosFuturos = {}
 
-dicionarioDeEstadosFuturos = {};
+for estado in estados:
+    dicionarioDeEstadosFuturos[estado] = {}  # criando um dicionario para cada estado
 
-for state in states:
-    dicionarioDeEstadosFuturos[state] = {} #criando um dicionario para cada estado
+for i in range(0, transacoes):
+    quintupla = input().split(" ") # pegando a tripla
+    estadoFrom = quintupla[0]
+    letra = quintupla[1]
+    desempilha = quintupla[2]
+    estadoTo = quintupla[3]
+    empilha = quintupla[4]
 
-for i in range(0, trasaction):
-    tripla = input().split(" ");  # pegando a tripla
-    stateFrom = tripla[0];
-    letra = tripla[1];
-    stateTo = tripla[2];
+    if letra not in dicionarioDeEstadosFuturos[estadoFrom]:  # adiciona um estado a lista de estados futuros
+        dicionarioDeEstadosFuturos[estadoFrom][letra] = []
 
-    if(letra in dicionarioDeEstadosFuturos[stateFrom]): #adiciona um estado a lista de estados futuros
-        dicionarioDeEstadosFuturos[stateFrom][letra].append(stateTo)
-    else: #ou cria a lista caso essa ainda não exista
-        dicionarioDeEstadosFuturos[stateFrom][letra] = [stateTo]
+    dicionarioDeEstadosFuturos[estadoFrom][letra].append([desempilha, estadoTo, empilha])
 
-stack_of_atualStates = []
+estadoInicial = input()
+estadosFinais = input().split(" ")
 
-initialState = input()
-finalStates = input().split(" ");
+palavras = input().split(" ")
 
-words = input().split(" ")
-
-for word in words:
-    stack_of_atualStates = [initialState]
-    for letter in word:
-        stack_of_futureStates = []
-        for state in stack_of_atualStates:
-            if(dicionarioDeEstadosFuturos[state].get(letter)):
-                futureStates = dicionarioDeEstadosFuturos[state].get(letter)
-                for futureState in futureStates:
-                    if(futureState not in stack_of_futureStates):
-                        stack_of_futureStates.append(futureState)
-        stack_of_atualStates = stack_of_futureStates
-
+for palavra in palavras:
+    pilhaEstadosAtuais = [(estadoInicial, [], palavra)]
     flag = 0
-    for state in stack_of_atualStates:
-        if(state in finalStates):
-            flag = 1
-    if(flag):
+
+    while flag == 0 and len(palavra) > 0 and len(pilhaEstadosAtuais) > 0:
+        estado, pilha, palavraTemp = pilhaEstadosAtuais.pop()
+
+        if dicionarioDeEstadosFuturos[estado].get('*') and palavraTemp != '*':
+            triplas = dicionarioDeEstadosFuturos[estado].get('*')
+            for desempilha, estadoTo, empilha in triplas:
+                pilhaTemporaria = pilha.copy()
+
+                if desempilha != '*':
+                    if len(pilhaTemporaria) == 0:
+                        break
+                    else:
+                        topo = pilhaTemporaria.pop()
+                        if desempilha != topo:
+                            continue
+
+                if empilha != '*':
+                    pilhaTemporaria.append(empilha)
+
+                pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria,palavraTemp])
+
+        if len(palavraTemp) == 0:
+            if estado in estadosFinais and len(pilha) == 0:
+                flag = 1
+                break
+            else:
+                continue
+
+        palavraTemp2 = palavraTemp[1:]
+
+        if dicionarioDeEstadosFuturos[estado].get(palavraTemp[0]):
+            triplas = dicionarioDeEstadosFuturos[estado].get(palavraTemp[0])
+            for desempilha, estadoTo, empilha in triplas:
+                pilhaTemporaria = pilha.copy()
+
+                if desempilha != '*':
+                    if len(pilhaTemporaria) == 0:
+                        break
+                    else:
+                        topo = pilhaTemporaria.pop()
+                        if desempilha != topo:
+                            continue
+
+                if empilha != '*':
+                    pilhaTemporaria.append(empilha)
+
+                pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria, palavraTemp2])
+
+    if flag:
         print('S')
     else:
         print('N')
