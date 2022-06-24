5a7cee580fb96884dda7545d7ae85d81e3ebaa50
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-06-23 22:49:31 -0300

    v1

--- AFND/main.py
+++ AFND/main.py
@@ -27,17 +27,16 @@ estadosFinais = input().split(" ")
 palavras = input().split(" ")
 
 for palavra in palavras:
-    pilha = []
-    pilhaEstadosAtuais = [(estadoInicial, pilha, palavra)]
+    pilhaEstadosAtuais = [(estadoInicial, [], palavra)]
     flag = 0
 
     while flag == 0 and len(palavra) > 0 and len(pilhaEstadosAtuais) > 0:
-        estado, pilhaTemp, palavraTemp = pilhaEstadosAtuais.pop()
+        estado, pilha, palavraTemp = pilhaEstadosAtuais.pop()
 
         if dicionarioDeEstadosFuturos[estado].get('*') and palavraTemp != '*':
             triplas = dicionarioDeEstadosFuturos[estado].get('*')
             for desempilha, estadoTo, empilha in triplas:
-                pilhaTemporaria = pilhaTemp.copy()
+                pilhaTemporaria = pilha.copy()
 
                 if desempilha != '*':
                     if len(pilhaTemporaria) == 0:
@@ -53,13 +52,13 @@ for palavra in palavras:
                 pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria,palavraTemp])
 
         if len(palavraTemp) == 0:
-            if estado in estadosFinais and len(pilhaTemp) == 0:
+            if estado in estadosFinais and len(pilha) == 0:
                 flag = 1
                 break
             else:
                 continue
 
-        palavraTemp2 = palavraTemp[1:0]
+        palavraTemp2 = palavraTemp[1:]
 
         if dicionarioDeEstadosFuturos[estado].get(palavraTemp[0]):
             triplas = dicionarioDeEstadosFuturos[estado].get(palavraTemp[0])
