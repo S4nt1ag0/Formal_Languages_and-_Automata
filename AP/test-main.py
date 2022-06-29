cb3741bffe3e0bf6ce9a947ffca58da2a138c1ee
S4nt1ag0 <gustavosantiago1018@gmail.com>
2022-06-29 00:39:11 -0300

    finalizando arquivo de testes automatizados

--- AP/test-main.py
+++ AP/test-main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 dicionarioDeEstadosFuturos = {
     "0":{
         "a":[['*','0','A']],
@@ -16,67 +17,78 @@ wb = 'b'
 
 palavras = []
 
-for i in range(5):
+for i in range(30):
    w = wa+wb
    palavras.append(w)
-   wa = wa*5
-   wb = wb*5
+   wa = wa+'a'
+   wb = wb+'b'
 
-for palavra in palavras:
-    pilhaEstadosAtuais = [(estadoInicial, [], palavra)]
-    flag = 0
+palavraAnalytics = {}
+for i in range(1000):
+    for palavra in palavras:
+        data1 = datetime.now()
+        time1 = datetime.timestamp(data1)
+        pilhaEstadosAtuais = [(estadoInicial, [], palavra)]
+        flag = 0
 
-    while flag == 0 and len(palavra) > 0 and len(pilhaEstadosAtuais) > 0:
-        estado, pilha, palavraTemp = pilhaEstadosAtuais.pop()
+        while flag == 0 and len(palavra) > 0 and len(pilhaEstadosAtuais) > 0:
+            estado, pilha, palavraTemp = pilhaEstadosAtuais.pop()
 
-        if dicionarioDeEstadosFuturos[estado].get('*') and palavraTemp != '*':
-            triplas = dicionarioDeEstadosFuturos[estado].get('*')
-            for desempilha, estadoTo, empilha in triplas:
-                pilhaTemporaria = pilha.copy()
+            if dicionarioDeEstadosFuturos[estado].get('*') and palavraTemp != '*':
+                triplas = dicionarioDeEstadosFuturos[estado].get('*')
+                for desempilha, estadoTo, empilha in triplas:
+                    pilhaTemporaria = pilha.copy()
 
-                if desempilha != '*':
-                    if len(pilhaTemporaria) == 0:
-                        break
-                    else:
-                        topo = pilhaTemporaria.pop()
-                        if desempilha != topo:
-                            continue
+                    if desempilha != '*':
+                        if len(pilhaTemporaria) == 0:
+                            break
+                        else:
+                            topo = pilhaTemporaria.pop()
+                            if desempilha != topo:
+                                continue
 
-                if empilha != '*':
-                    for element in empilha:
-                        pilhaTemporaria.append(element)
+                    if empilha != '*':
+                        for element in empilha:
+                            pilhaTemporaria.append(element)
 
-                pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria, palavraTemp])
+                    pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria, palavraTemp])
 
-        if len(palavraTemp) == 0:
-            if estado in estadosFinais and len(pilha) == 0:
-                flag = 1
-                break
-            else:
-                continue
+            if len(palavraTemp) == 0:
+                if estado in estadosFinais and len(pilha) == 0:
+                    flag = 1
+                    break
+                else:
+                    continue
 
-        palavraTemp2 = palavraTemp[1:]
+            palavraTemp2 = palavraTemp[1:]
 
-        if dicionarioDeEstadosFuturos[estado].get(palavraTemp[0]):
-            triplas = dicionarioDeEstadosFuturos[estado].get(palavraTemp[0])
-            for desempilha, estadoTo, empilha in triplas:
-                pilhaTemporaria = pilha.copy()
+            if dicionarioDeEstadosFuturos[estado].get(palavraTemp[0]):
+                triplas = dicionarioDeEstadosFuturos[estado].get(palavraTemp[0])
+                for desempilha, estadoTo, empilha in triplas:
+                    pilhaTemporaria = pilha.copy()
 
-                if desempilha != '*':
-                    if len(pilhaTemporaria) == 0:
-                        break
-                    else:
-                        topo = pilhaTemporaria.pop()
-                        if desempilha != topo:
-                            continue
+                    if desempilha != '*':
+                        if len(pilhaTemporaria) == 0:
+                            break
+                        else:
+                            topo = pilhaTemporaria.pop()
+                            if desempilha != topo:
+                                continue
 
-                if empilha != '*':
-                    for element in empilha:
-                        pilhaTemporaria.append(element)
+                    if empilha != '*':
+                        for element in empilha:
+                            pilhaTemporaria.append(element)
 
-                pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria, palavraTemp2])
+                    pilhaEstadosAtuais.append([estadoTo, pilhaTemporaria, palavraTemp2])
 
-    if flag:
-        print('S')
-    else:
-        print('N')
+        data2 = datetime.now()
+        time2 = datetime.timestamp(data2)
+        if not palavraAnalytics.get(palavra):
+            palavraAnalytics[palavra] = 0.0
+
+        palavraAnalytics[palavra] += (time2-time1)
+
+for palavra in palavraAnalytics.keys():
+    palavraAnalytics[palavra]=palavraAnalytics[palavra]/1000;
+    print(palavraAnalytics[palavra])
+
